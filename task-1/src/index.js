9dd893658bb37c02bc7947678e74c25fcf41d5fe
jaxin007 <swatdimonbil@gmail.com>
2019-11-21 14:24:28 +0200

    Update homework-01

--- task-1/src/index.js
+++ task-1/src/index.js
@@ -4,11 +4,12 @@ const nanoid = require("nanoid");
 const hostname = "127.0.0.1";
 const port = 3000;
 
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
   res.end(nanoid());
 });
 
-server.listen(port, hostname, () => console.log(`Server is running at http://${hostname}:${port}/`));
+server.listen(port, hostname, () =>
+  console.log(`Server is running at http://${hostname}:${port}/`)
+);
