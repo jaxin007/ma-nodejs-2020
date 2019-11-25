4788f60f5f9158af704818d84da0c762cbe1c07e
Dima Bilozor <swatdimonbil@gmail.com>
2019-11-25 22:03:54 +0200

    updated app.js

--- task-2/app.js
+++ task-2/app.js
@@ -1,9 +1,7 @@
 const { sum: lection, earth: spaceObject, setTimeOutPromise } = require('./src');
 
-const result = lection(1, 3, 3);
-
 async function boot() {
-  console.log(result);
+  console.log(lection(1, 3, 3));
   const secResult = await setTimeOutPromise(3000, 'some text');
   console.log(secResult);
   console.log(spaceObject.toString());
