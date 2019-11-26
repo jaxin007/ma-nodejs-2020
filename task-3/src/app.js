b2034d9a729378d2e4526383bc8aa620ffacbf35
Dima Bilozor <swatdimonbil@gmail.com>
2019-11-26 22:10:07 +0200

    updated

--- task-3/src/app.js
+++ task-3/src/app.js
@@ -1,8 +1,8 @@
 function isPrime(num) {
   if (num <= 1) return false; // negatives
-  if (num % 2 == 0 && num > 2) return false; // even numbers
+  if (num % 2 === 0 && num > 2) return false; // even numbers
   const s = Math.sqrt(num); // store the square to loop faster
-  for (let i = 3; i <= s; i++) {
+  for (let i = 3; i <= s; i += 1) {
     // start from 3, stop at the square, increment
     if (num % i === 0) return false; // modulo shows a divisor was found
   }
@@ -10,14 +10,9 @@ function isPrime(num) {
 }
 
 let theBiggestNumber = 0;
-let isLoopEnded = false;
 const date = +new Date();
 
-setTimeout(() => {
-  isLoopEnded = true;
-}, 1000);
-
-for (let i = 0; ; i++) {
+for (let i = 0; ; i += 1) {
   if (isPrime(i)) {
     theBiggestNumber = i;
   }
