f113c576b398bfe466d45e01a2decfa6424835cf
jaxin007 <swatdimonbil@gmail.com>
2019-12-05 14:31:53 +0200

    added async and promise task to my hw-04

--- task-4/src/promises.js
+++ task-4/src/promises.js
@@ -1,5 +1,34 @@
-// const { throwDice } = require('./app');
+const { throwDice } = require('./app');
 
-// let firstDice;
-// let secondDice;
-// let sum;
+let firstDice;
+let secondDice;
+let sum;
+
+function throwDiceAfter(timeInMillis) {
+  const nameOfVariable = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      nameOfVariable = throwDice();
+      if (nameOfVariable === 0) {
+        return reject(new Error('Dice lost'));
+      }
+      resolve(nameOfVariable);
+    }, timeInMillis);
+  });
+}
+
+const secondThrow = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    secondDice = throwDice();
+    if (secondDice === 0) {
+      return reject(new Error('Dice lost'));
+    }
+    resolve(secondDice);
+  }, 2000);
+});
+
+const result = new Promise((resolve) => {
+  setTimeout(() => {
+    sum = firstDice + secondDice;
+    resolve(sum);
+  }, 3000);
+});
