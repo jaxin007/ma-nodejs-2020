9f70699d73497685a9d123df66b68e2b7a9b8c6c
Dima Bilozor <swatdimonbil@gmail.com>
2020-01-11 20:33:24 +0200

    updated hw-06

--- task-6/app.js
+++ task-6/app.js
@@ -2,7 +2,9 @@ const { memoryMonitor } = require('./src/index');
 
 // $env:RATE = 300;$env:LIMIT = 15000;$env:COLOR = 'false'; node app.js - start application with RATE = Value
 
-const rate = process.env.RATE === 1000;
-const limit = process.env.LIMIT === 15000;
-const color = process.env.COLOR !== 'false';
-memoryMonitor(rate, limit, color);
+const rate = process.env.RATE || 1000;
+const RATE = JSON.parse(rate);
+const limit = process.env.LIMIT || 10000;
+const colorValue = process.env.COLOR === 'false';
+const color = JSON.parse(colorValue);
+memoryMonitor(RATE, limit, color);
--- task-6/src/index.js
+++ task-6/src/index.js
@@ -3,48 +3,50 @@ const os = require('os');
 const whiteColor = '\x1b[37m';
 let greenColor = '\x1b[32m';
 let redColor = '\x1b[31m';
-let defaultColor = '\x1b[37m';
 // colores
 
 function memoryMonitor(rate, limit, color) {
   let lastFreeMem = os.freemem();
-  if (os.freemem() > limit) {
-    defaultColor = redColor;
-    redColor = greenColor;
-  }
+
   setInterval(() => {
-    os.freemem();
-    os.totalmem();
     console.clear();
     const totalMem = os.totalmem() / (1024 * 1024);
     const freeMem = os.freemem() / (1024 * 1024);
     const allocatedMem = totalMem - freeMem;
     const delta = freeMem - lastFreeMem;
     lastFreeMem = freeMem;
+
     if (color === false) {
       redColor = whiteColor;
-      defaultColor = whiteColor;
       greenColor = whiteColor;
     }
-    if (delta < 0) {
-      greenColor = '\x1b[31m'; // change green color to red if delta < 0
-    } else {
-      greenColor = '\x1b[32m'; // return green color if delta > 0
-    }
     console.log(`Total memory available: ${totalMem.toFixed(3)}, MB`);
-    console.log(`Free memory available:${defaultColor} ${freeMem.toFixed(3)} ${whiteColor}MB`);
+
+    if (freeMem > limit) {
+      console.log(`Free memory available:${whiteColor} ${freeMem.toFixed(3)} ${whiteColor}MB`);
+    } else {
+      console.log(`Free memory available:${redColor} ${freeMem.toFixed(3)} ${whiteColor}MB`);
+    }
     console.log(`Allocated memory: ${allocatedMem.toFixed(3)}MB`);
-    console.log(
-      `Delta for previous allocated mamory value: ${greenColor} ${delta} ${whiteColor}MB`,
-    );
+
+    if (delta < 0) {
+      console.log(
+        `Delta for previous allocated mamory value: ${redColor} ${delta} ${whiteColor}MB`,
+      );
+    } else {
+      console.log(
+        `Delta for previous allocated mamory value: ${greenColor} ${delta} ${whiteColor}MB`,
+      );
+    }
+
     if (freeMem < limit) {
       console.log(
-        `${'\x1b[31m'}!!! ATTENTION: Available memory is under the defined limit !!!${whiteColor}`,
+        `${redColor}!!! ATTENTION: Available memory is under the defined limit !!!${whiteColor}`,
       );
     }
   }, rate);
 }
-
+memoryMonitor(1000, 10000, 'false');
 module.exports = {
   memoryMonitor,
 };
