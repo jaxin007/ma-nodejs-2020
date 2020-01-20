6bc6cc661b37b267f78de2293d7874648aa5678a
jaxin007 <swatdimonbil@gmail.com>
2020-01-20 13:34:07 +0200

    updated hw-07

--- task-7/src/server.js
+++ task-7/src/server.js
@@ -1,4 +1,24 @@
-// const http = require('http');
-// // const { memoryMonitor } = require('./index');
+const http = require('http');
+const { memoryMonitor } = require('./memoryMonitor');
 
-// // const server = http.createServer();
+const port = 8000;
+http
+  .createServer(function(req, res) {
+    const { method, url, baseUrl } = req;
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    switch (url) {
+      case '/':
+        res.write('Server started');
+        res.end();
+        break;
+      case '/user':
+        if (method === 'GET' && baseUrl === '/limit') res.write(JSON.stringify(memoryMonitor));
+        res.end();
+        break;
+      default:
+        break;
+    }
+  })
+  .listen(port);
+console.log(`server started with port ${port}`);
