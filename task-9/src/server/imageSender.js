cf22bb2ae18e7752de59ecce6ddb462b5e15a31d
Dima Bilozor <swatdimonbil@gmail.com>
2020-02-18 09:51:57 +0200

    updated

--- task-9/src/server/imageSender.js
+++ task-9/src/server/imageSender.js
@@ -1,14 +1,42 @@
-const fsp = require('fs').promises;
+/* eslint-disable no-underscore-dangle */
+const fs = require('fs');
+const { pipeline, Transform } = require('stream');
 const config = require('../config');
+const { endResponse } = require('./helpers');
+
+class DotsGenerator extends Transform {
+  // eslint-disable-next-line no-useless-constructor
+  constructor() {
+    super();
+  }
+
+  _transform(chunk, encoding, next) {
+    process.stdout.write('.');
+    this.push(chunk);
+    setTimeout(() => {
+      next();
+    }, 1000);
+  }
+}
 
 function sendJPEG(res) {
-  fsp
-    .readFile(config.filePath)
-    .then((buffer) => res.end(buffer))
-    .catch((err) => {
-      console.error('Failed to send image buffer!', err.stack);
-      res.emit('error', new Error('Failed to send image!'));
-    });
+  const readStream = fs.createReadStream(config.filePath, {
+    highWaterMark: config.rate,
+  });
+
+  readStream.on('error', () => {
+    endResponse(res, 500);
+  });
+
+  const slowerTransform = new DotsGenerator();
+
+  pipeline(readStream, slowerTransform, res, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(`\nStatus: ${res.statusMessage}`);
+  });
 }
 
 module.exports = {
