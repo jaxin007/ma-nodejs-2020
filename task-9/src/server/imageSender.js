07bcf8c0fcc1e0bf3040ff558d12a3bf43ebfa29
Dima Bilozor <swatdimonbil@gmail.com>
2020-02-19 08:03:40 +0200

    update imageSender

--- task-9/src/server/imageSender.js
+++ task-9/src/server/imageSender.js
@@ -5,40 +5,46 @@ const config = require('../config');
 const { endResponse } = require('./helpers');
 
 class DotsGenerator extends Transform {
-  // eslint-disable-next-line no-useless-constructor
-  constructor() {
-    super();
-  }
+	// eslint-disable-next-line no-useless-constructor
+	constructor() {
+		super();
+		this.length = 0;
+	}
 
-  _transform(chunk, encoding, next) {
-    process.stdout.write('.');
-    this.push(chunk);
-    setTimeout(() => {
-      next();
-    }, 1000);
-  }
+	_transform(chunk, encoding, next) {
+		let fileSize = (this.length += chunk.length) / 1024;
+		// console.log(fileSize);
+		if (fileSize >= 1024) {
+			process.stdout.write('.');
+			fileSize = 0;
+		}
+		this.push(chunk);
+		setTimeout(() => {
+			next();
+		}, 100);
+	}
 }
 
 function sendJPEG(res) {
-  const readStream = fs.createReadStream(config.filePath, {
-    highWaterMark: config.rate,
-  });
+	const readStream = fs.createReadStream(config.filePath, {
+		highWaterMark: config.rate
+	});
 
-  readStream.on('error', () => {
-    endResponse(res, 500);
-  });
+	readStream.on('error', () => {
+		endResponse(res, 500);
+	});
 
-  const slowerTransform = new DotsGenerator();
+	const slowerTransform = new DotsGenerator();
 
-  pipeline(readStream, slowerTransform, res, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`\nStatus: ${res.statusMessage}`);
-  });
+	pipeline(readStream, slowerTransform, res, (err) => {
+		if (err) {
+			console.error(err);
+			return;
+		}
+		console.log(`\nStatus: ${res.statusMessage}`);
+	});
 }
 
 module.exports = {
-  sendJPEG,
+	sendJPEG
 };
